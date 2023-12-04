55d791fa238d8c15c8d4a9c12ca895a8d9bc4b0e
ssisworo.work <ssisworo.work@gmail.com>
2023-12-04 14:32:16 +0700

    adding test crud

---
+++ app/src/main/java/com/lunix/studysync/register.java
@@ -0,0 +1,14 @@
+package com.lunix.studysync;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
