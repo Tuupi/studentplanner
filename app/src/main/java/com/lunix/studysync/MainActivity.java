8c6eb7290409bbcbb05df8351c063a7515d15622
ssisworo.work <ssisworo.work@gmail.com>
2023-12-04 15:27:57 +0700

    adding test crud

--- app/src/main/java/com/lunix/studysync/MainActivity.java
+++ app/src/main/java/com/lunix/studysync/MainActivity.java
@@ -2,7 +2,9 @@ package com.lunix.studysync;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button loginBtn = (Button)findViewById(R.id.loginBtn);
 
-
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent switchActivityIntent = new Intent(MainActivity.this, crudtest.class);
+                startActivity(switchActivityIntent);
+            }
+        }
+        );
     }
 }
