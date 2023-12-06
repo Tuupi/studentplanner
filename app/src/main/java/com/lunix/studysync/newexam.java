55b6472b49992fc11d9e09b5f57610e9c34b2269
zahra <zahra.annisa.fakhira.tik22@gmail.com>
2023-12-06 23:37:15 +0700

    UI

---
+++ app/src/main/java/com/lunix/studysync/newexam.java
@@ -0,0 +1,14 @@
+package com.lunix.studysync;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newexam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newexam);
+    }
+}
