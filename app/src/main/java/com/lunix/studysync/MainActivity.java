17bd659a59fde19240aeb93f094c4803c773aa91
ssisworo.work <ssisworo.work@gmail.com>
2023-12-07 14:21:33 +0700

    making sign in

--- app/src/main/java/com/lunix/studysync/MainActivity.java
+++ app/src/main/java/com/lunix/studysync/MainActivity.java
@@ -1,28 +1,102 @@
 package com.lunix.studysync;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    EditText email, passwordlogin;
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button loginBtn = (Button)findViewById(R.id.loginBtn);
+        email = findViewById(R.id.email);
+        passwordlogin = findViewById(R.id.password);
+
+        // ...
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent switchActivityIntent = new Intent(MainActivity.this, crudtest.class);
-                startActivity(switchActivityIntent);
+                String emailStr, passwordStr;
+                emailStr = email.getText().toString();
+                passwordStr = passwordlogin.getText().toString();
+
+                if(TextUtils.isEmpty(emailStr)){
+                    Toast.makeText(MainActivity.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                } else
+                if(TextUtils.isEmpty(passwordStr)){
+                    Toast.makeText(MainActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                signIn(emailStr, passwordStr);
+
+
+//                Intent switchActivityIntent = new Intent(MainActivity.this, crudtest.class);
+//                startActivity(switchActivityIntent);
             }
         }
         );
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Toast.makeText(MainActivity.this, "Cannot find your account", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    private void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent switchActivityIntent = new Intent(MainActivity.this, crudtest.class);
+                            startActivity(switchActivityIntent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+    private void reload() { }
+
+    private void updateUI(FirebaseUser user) {
 
     }
 }
