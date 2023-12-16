563c5a21ad5a043e09cef72d4009ce4ef2da4785
zahra <zahra.annisa.fakhira.tik22@gmail.com>
2023-12-16 12:20:44 +0700

    testcapek

--- app/src/main/java/com/lunix/studysync/TaskFragment.java
+++ app/src/main/java/com/lunix/studysync/TaskFragment.java
@@ -1,28 +1,46 @@
 package com.lunix.studysync;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link SubjectFragment#newInstance} factory method to
+ * Use the {@link TaskFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class TaskFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private DatabaseReference databaseUsers;
+
+    RecyclerView recyclerView;
+    ArrayList<Mytask> list;
+    DatabaseReference databaseReference;
+    TaskAdapter taskAdapter;
 
     public TaskFragment() {
         // Required empty public constructor
@@ -34,11 +52,10 @@ public class TaskFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment SubjectFragment.
+     * @return A new instance of fragment TaskFragment.
      */
-    // TODO: Rename and change types and number of parameters
-    public static SubjectFragment newInstance(String param1, String param2) {
-        SubjectFragment fragment = new SubjectFragment();
+    public static TaskFragment newInstance(String param1, String param2) {
+        TaskFragment fragment = new TaskFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -53,12 +70,96 @@ public class TaskFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        databaseUsers = FirebaseDatabase.getInstance().getReference();
+
+//        recyclerView = findViewById(R.id.recycleview);
+//        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+//        list = new ArrayList<>();
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        taskAdapter = new TaskAdapter(this, list);
+//        recyclerView.setAdapter(taskAdapter);
+//
+//        databaseReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+//                    Mytask mytask = dataSnapshot.getValue(Mytask.class);
+//                    list.add(mytask);
+//                }
+//                taskAdapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_task, container, false);
+        recyclerView = rootView.findViewById(R.id.recycleview);
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+        list = new ArrayList<>();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        taskAdapter = new TaskAdapter(this, list);
+        recyclerView.setAdapter(taskAdapter);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    Mytask mytask = dataSnapshot.getValue(Mytask.class);
+                    list.add(mytask);
+                }
+                taskAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return inflater.inflate(R.layout.fragment_subject, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task, container, false);
+//        View rootView = inflater.inflate(R.layout.fragment_task, container, false);
+//        Button create = rootView.findViewById(R.id.create);
+//        Button view = rootView.findViewById(R.id.view);
+//        EditText name = rootView.findViewById(R.id.TaskName);
+//        EditText course = rootView.findViewById(R.id.CourseName);
+//        EditText date = rootView.findViewById(R.id.Date);
+//
+//        create.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                InsertData(name.getText().toString(), course.getText().toString(), date.getText().toString());
+//            }
+//        });
+//
+//        view.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(getActivity(), Tasklist.class));
+//                getActivity().finish();
+//            }
+//        });
+//
+//        return rootView;
+//    }
+//
+//    private void InsertData(String taskname, String taskcourse, String taskdate) {
+//        String id = databaseUsers.push().getKey();
+//        Mytask mytask = new Mytask(taskname, taskcourse, taskdate);
+//        databaseUsers.child("users").child(id).setValue(mytask)
+//                .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        if (task.isSuccessful()) {
+//                            Toast.makeText(getActivity(), "Task details inserted", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
     }
 }
