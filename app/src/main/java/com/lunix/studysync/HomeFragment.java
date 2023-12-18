795e73214c4bde930d9d052761171848c159187d
ssisworo.work <ssisworo.work@gmail.com>
2023-12-19 01:39:37 +0700

    add home

--- app/src/main/java/com/lunix/studysync/HomeFragment.java
+++ app/src/main/java/com/lunix/studysync/HomeFragment.java
@@ -2,11 +2,24 @@ package com.lunix.studysync;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,10 +32,22 @@ public class HomeFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "HomeFragment";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    FirebaseAuth mAuth;
+    String user;
+    ArrayList<Task> tasklist;
+    DatabaseReference taskReference;
+    TaskAdapter taskAdapter;
+    ArrayList<Exam> examList;
+    DatabaseReference examReference;
+    ExamAdapter examAdapter;
+    ArrayList<Subject> subjectList;
+    DatabaseReference subjectReference;
+    SubjectAdapter subjectAdapter;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -49,6 +74,9 @@ public class HomeFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -59,6 +87,95 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        TextView amountCourse = rootView.findViewById(R.id.amountCourse);
+        TextView amountTask = rootView.findViewById(R.id.amountTask);
+        TextView amountExam = rootView.findViewById(R.id.amountExam);
+        String courseamt;
+        String taskamt;
+        taskReference = FirebaseDatabase.getInstance().getReference("users").child(user).child("task");
+        tasklist = new ArrayList<>();
+        taskAdapter = new TaskAdapter(requireContext(), tasklist);
+
+        taskReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                tasklist.clear();
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    Log.d(TAG, "Test nama : " + dataSnapshot.getValue(Task.class).getName());
+                    Log.d(TAG, "Test nama : " + dataSnapshot.getValue(Task.class).getCourse());
+                    Log.d(TAG, "Test nama : " + dataSnapshot.getValue(Task.class).getDate());
+                    Task task = dataSnapshot.getValue(Task.class);
+                    tasklist.add(task);
+
+                }
+                taskAdapter.notifyDataSetChanged();
+                amountTask.setText(Integer.toString(taskAdapter.getItemCount()));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+        });
+        examReference = FirebaseDatabase.getInstance().getReference("users").child(user).child("exams");
+        examList = new ArrayList<>();
+        examAdapter = new ExamAdapter(requireContext(), examList);
+        examReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                examList.clear();
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    Log.d(TAG, "Test nama : " + dataSnapshot.getValue(Exam.class).getName());
+                    Log.d(TAG, "Test nama : " + dataSnapshot.getValue(Exam.class).getCourse());
+                    Log.d(TAG, "Test nama : " + dataSnapshot.getValue(Exam.class).getDate());
+                    Exam exam = dataSnapshot.getValue(Exam.class);
+                    examList.add(exam);
+                }
+                examAdapter.notifyDataSetChanged();
+                amountExam.setText(Integer.toString(examAdapter.getItemCount()));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+        });
+        subjectReference = FirebaseDatabase.getInstance().getReference("users").child(user).child("courses");
+        subjectList = new ArrayList<>();
+        subjectAdapter = new SubjectAdapter(requireContext(), subjectList);
+        subjectReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                subjectList.clear();
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    Log.d(TAG, "Test nama : " + dataSnapshot.getValue(Subject.class).getCourse());
+                    Log.d(TAG, "Test nama : " + dataSnapshot.getValue(Subject.class).getDate());
+                    Subject subject = dataSnapshot.getValue(Subject.class);
+                    subjectList.add(subject);
+
+                }
+                subjectAdapter.notifyDataSetChanged();
+                amountCourse.setText(Integer.toString(subjectAdapter.getItemCount()));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+        });
+        Log.d(TAG, "onCreateView: " + subjectAdapter.getItemCount());
+        if (!subjectAdapter.list.isEmpty()) {
+
+        } if(!taskAdapter.list.isEmpty()) {
+
+        } if (!examAdapter.list.isEmpty()) {
+
+        }
+        Log.d(TAG, "test");
+        return rootView;
     }
 }
